61f97036e629004ae70b9e99f6a6577ef72c343e
OjeshManandhar <ozes.manandhar@gmail.com>
2019-06-16 10:53:16 +0545

    Add Sign Out Button

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
+
+import NavBar from './../components/NavBar';
+import Button from './../components/Button';
 
 class Profile extends React.Component {
+    SignOut() {
+        console.log('Sign Out');
+        Alert.alert(
+            'Sign Out',
+            'You have pressed Sign Out',
+            [
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: false }
+          );
+    }
 
+    render() {
+        return(
+            <View style={styles.container}>
+                <View style={styles.contentStyle}>
+                    <Button onPress={() => this.SignOut()}>Sign Out</Button>
+                </View>
+                <View style={styles.navBarStyle}>
+                    <NavBar navigation={this.props.navigation} selected='Profile' />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    contentStyle: {
         flex: 1,
-        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    navBarStyle: {
+        height: 50
     }
 });
 
