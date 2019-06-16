20e3fd05d9cfe7c970e75d31f39a41abca8f4ff8
OjeshManandhar <ozes.manandhar@gmail.com>
2019-06-16 10:19:16 +0545

    correct error in SignUp.js

---
+++ src/screens/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+class SignUp extends React.Component {
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default SignUp;
