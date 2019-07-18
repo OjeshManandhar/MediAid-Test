f0c352e69731d37848a6c9265b7319320df2938c
OjeshManandhar <ozes.manandhar@gmail.com>
2019-07-18 15:31:55 +0545

    Add boilerplate

---
+++ src/screens/Database.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {} from 'react-native';
+
+import Button from './../components/Button';
+import LoadingButton from './../components/LoadingButton';
+
+class Database extends React.Component {
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    content: {
+        flex: 1
+    },
+    navBarStyle: {
+        height: 50
+    }
+});
+
+export default Database;
