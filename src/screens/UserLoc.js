7a0f5b1f07ea3fd84ba70f80bc15a3c84e2cd1cf
OjeshManandhar <ozes.manandhar@gmail.com>
2019-07-30 16:48:59 +0545

    Copy paste code
    from: https://www.tutorialspoint.com/react_native/react_native_geolocation

--- src/screens/UserLoc.js
+++ src/screens/UserLoc.js
@@ -1,47 +1,61 @@
-import React from 'react';
-import {
-    View,
-    Text,
-    PermissionsAndroid,
-    StyleSheet
-} from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, Switch, StyleSheet} from 'react-native'
 
-class UserLoc extends React.Component {
-    static navigationOptions = {
-        header: undefined,
-        title: 'User Location'
-    };
-
-    // For asking permission
-    // import 'PermissionsAndroid' from 'react-native'
-    componentDidMount() {
-        PermissionsAndroid.requestMultiple(
-            [PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION],
-            {
-                title: 'Give Location Permission',
-                message: 'App needs location permission to find your position.'
-            }
-        ).then(granted => {
-            console.log(granted);
-        }).catch(err => {
-            console.warn(err);
+class SwichExample extends Component {
+    state = {
+        initialPosition: 'unknown',
+        lastPosition: 'unknown',
+    }
+    watchID: ?number = null;
+    componentDidMount = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+            const initialPosition = JSON.stringify(position);
+            this.setState({ initialPosition });
+            },
+            (error) => alert(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+        this.watchID = navigator.geolocation.watchPosition((position) => {
+            const lastPosition = JSON.stringify(position);
+            this.setState({ lastPosition });
         });
     }
-    
+    componentWillUnmount = () => {
+        navigator.geolocation.clearWatch(this.watchID);
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>UserLoc</Text>
+            <View style = {styles.container}>
+            <Text style = {styles.boldText}>
+                Initial position:
+            </Text>
+            
+            <Text>
+                {this.state.initialPosition}
+            </Text>
+            
+            <Text style = {styles.boldText}>
+                Current position:
+            </Text>
+            
+            <Text>
+                {this.state.lastPosition}
+            </Text>
             </View>
-        );
+        )
     }
 }
+export default SwichExample
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
     container: {
-        flex: 1
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 50
+    },
+    boldText: {
+        fontSize: 30,
+        color: 'red',
     }
-});
-
-export default UserLoc;
+})
